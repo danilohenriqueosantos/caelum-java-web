a1cb4caf3f15788303ee16d7e9e3940c224568b9
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-09-07 15:13:53 -0300

    Implementando tela de cadastro de contato

---
+++ fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -0,0 +1,63 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.caelum.agenda.dao.ContatoDao;
+import br.com.caelum.agenda.modelo.Contato;
+
+@WebServlet("/adicionaContato")
+public class AdicionaContatoServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		PrintWriter out = resp.getWriter();
+
+		String nome = req.getParameter("nome");
+		String email = req.getParameter("email");
+		String endereco = req.getParameter("endereco");
+		String dataEmTexto = req.getParameter("dataNascimento");
+
+		Calendar dataNascimento = null;
+
+		try {
+			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+		} catch (Exception e) {
+			out.print("Erro na conversão da data");
+			return;
+		}
+
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEmail(email);
+		contato.setEndereco(endereco);
+		contato.setDataNascimento(dataNascimento);
+
+		ContatoDao contatoDao = new ContatoDao();
+		contatoDao.adiciona(contato);
+
+		out.print("<html>");
+		out.print("<body>");
+		out.print("Contat " + contato.getNome() + " adicionado com sucesso");
+		out.print("</body>");
+		out.print("</html>");
+
+	}
+}
