422bdae7cc9f527adb74424380d5735999b7ec07
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-09-09 12:46:16 -0300

    Finalizando a ConnectionFactory

--- fj21-agenda/src/br/com/caelum/agenda/ConnectionFactory.java
+++ fj21-agenda/src/br/com/caelum/agenda/ConnectionFactory.java
@@ -2,21 +2,49 @@ package br.com.caelum.agenda;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-	public Connection getConnection() throws SQLException {
-		System.out.println("conectando ...");
-
+	public static Connection getConnection() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			throw new SQLException(e);
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/fj21?useSSL=false", "root", "Da260716@!");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
-		
-		return DriverManager.getConnection("jdbc:mysql://localhost/fj21",
-				"root", "");
 	}
 
+	public static void closeConnection(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void closeConnection(Connection conn, PreparedStatement stmt) {
+		try {
+			closeConnection(conn);
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void closeConnection(Connection conn, PreparedStatement stmt, ResultSet rs) {
+		try {
+			closeConnection(conn, stmt);
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
