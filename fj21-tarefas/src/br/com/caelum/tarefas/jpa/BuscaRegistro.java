7841a725fd3caefb1ff6b097522149c963bd3903
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-09-28 15:45:48 -0300

    Buscando os registros na base de dados

--- fj21-tarefas/src/br/com/caelum/tarefas/jpa/BuscaRegistro.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/jpa/BuscaRegistro.java
@@ -1,32 +1,14 @@
 package br.com.caelum.tarefas.jpa;
 
-import java.util.Calendar;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.caelum.tarefas.modelo.Tarefa;
 
-public class GeraTabelas {
-
+public class BuscaRegistro {
 	
 	public static void main(String[] args) {
-		
-//		Tarefa tarefa = new Tarefa();
-//		tarefa.setDescricao("Estudar JPA");
-//		tarefa.setFinalizado(true);
-//		tarefa.setDataFinalizacao(Calendar.getInstance());
-//		
-//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tarefas");
-//		EntityManager manager = factory.createEntityManager();
-//		
-//		manager.getTransaction().begin();
-//		manager.persist(tarefa);
-//		manager.getTransaction().commit();
-//		
-//		manager.close();
-//		factory.close();
 		
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tarefas");
 		EntityManager manager = factory.createEntityManager();
@@ -36,6 +18,6 @@ public class GeraTabelas {
 		
 		manager.close();
 		factory.close();
-		
 	}
+
 }
